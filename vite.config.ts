9a5a8fbde20f966d445a6aad941b0dd2cfdb1e3e
Waseem Baggash Mudhsh <64817038+Waseem-Mudhsh@users.noreply.github.com>
2025-12-31 03:02:45 +0300

    Update vite.config.ts
    add this line :
    base: '/Visual-Identity-Guide-of-Apache/', // يجب أن يكون هكذا بالضبط بين شرطتين مائلتين

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Visual-Identity-Guide-of-Apache/', // يجب أن يكون هكذا بالضبط بين شرطتين مائلتين
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
